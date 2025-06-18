bda1cbf5ddfd0e401717b756aaf907ee831a23fb
Eldon Hayes <eldon.hayes.eh@gmail.com>
2025-06-17 22:33:01 -0700

    Initial DSO-Vulnhunter commit

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+# SQLi Ex. (OWASP A03)
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        query = f"SELECT * FROM users WHERE username='{username}' AND password = '{password}'"
+        return f"<p>Running query: {query}</p>"
+    return '''
+        <form method="post">
+            Username: <input name="username"><br>
+            Password: <input name="password"><br>
+            <input type="submit">
+        </form>
+    '''
+
+# XSS Ex. (OWASP A03)
+@app.route('/search')
+def search():
+    q = request.args.get('q', '')
+    return render_template_string(f"<h2> Results for: {q}</h2>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
